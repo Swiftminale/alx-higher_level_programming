96759c64145da200c1a918e52147e8aec1a687c4
Swiftminale <minalefetene@gmail.com>
2024-01-08 11:02:51 -0800

    first commit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
-def print_list_integer(my_list=]):
-	for num in my_list:
-		print("{:d}".format(num))
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print('{:d}'.format(i))
