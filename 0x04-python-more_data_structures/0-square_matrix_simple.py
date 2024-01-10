e4d6d27efc3ca757f4226416852fd7c134d7da93
Swiftminale <minalefetene@gmail.com>
2024-01-10 02:40:58 -0800

    first commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-def squared(element):
-    return (element**2)
-
 
 def square_matrix_simple(matrix=[]):
-    new = matrix[:]
-    for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                new[i] = list(map(squared, matrix[i]))
-    return new
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
