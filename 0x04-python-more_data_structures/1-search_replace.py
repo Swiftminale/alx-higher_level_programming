adcaad305b172ae10fdad9dea00c3937e9656520
Swiftminale <minalefetene@gmail.com>
2024-01-10 02:33:28 -0800

    first commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+# 1-search_replace.py
+# Brennan D Baraban <375@holbertonschool.com
+
+
 def search_replace(my_list, search, replace):
-    def find_search(element):
-        return element if element != search else replace
-    return list(map(find_search, my_list)i)
+    """Replace all occurrences of an element by another in a new list."""
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
