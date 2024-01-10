0db2900819cfbc57dd1b7b1fc1f09dc377a88171
Swiftminale <minalefetene@gmail.com>
2024-01-10 02:45:30 -0800

    first commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    return [list(map(lambda x: x ** 2, row)) for row in matrix]
+    result_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+    
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            result_matrix[i][j] = matrix[i][j] ** 2
+    
+    return result_matrix
